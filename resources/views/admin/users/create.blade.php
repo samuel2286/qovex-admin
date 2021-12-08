088b2ba3d527bf10554b2b46f5c9313c84c60fcd
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-12-08 23:39:40 +0000

    updated with form builder

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @push('page-css')
-    
+    <link rel="stylesheet" href="{{asset('assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css')}}">
 @endpush
 
 @section('content')
@@ -10,49 +10,13 @@
         <div class="card-body">
             <h4 class="card-title">Create User</h4>
 
-            <form action="{{route('users.store')}}" method="post" enctype="multipart/form-data">
-                @csrf
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input id="name" type="text" class="form-control" name="name" placeholder="Enter name">
-                </div>
-                <div class="form-group">
-                    <label for="username">Username</label>
-                    <input id="username" type="text" class="form-control" name="username" placeholder="Enter username">
-                </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input id="email" type="email" class="form-control" name="email" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                    <label for="role">Role</label>
-                    <select name="role" id="role" class="form-control">
-                        @foreach ($roles as $role)
-                        <option>{{$role->name}}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input id="password" type="password" class="form-control" name="password" placeholder="Enter password">
-                </div>
-                <div class="form-group">
-                    <label for="confirm_password">Confirm Password</label>
-                    <input id="confirm_password" type="password" class="form-control" name="password_confirmation" placeholder="Repeat password">
-                </div>
-                <div class="form-group">
-                    <label for="avatar">Avatar</label>
-                    <input type="file" name="avatar" class="form-control" id="avatar">
-                </div>
-                <div class="form-group">
-                    <button class="btn btn-primary btn-block" type="submit">Submit</button>
-                </div>
-            </form>
+            {!! form($form) !!}
         </div>
     </div>
 </div>
 @endsection
 
 @push('page-js')
-    
+    <script src="{{asset('assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js')}}"></script>
+    <script src="{{asset('assets/js/pages/form-advanced.init.js')}}"></script>
 @endpush
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -8,47 +8,8 @@
 <div class="col-12">
     <div class="card">
         <div class="card-body">
-            <h4 class="card-title">Create User</h4>
-
-            <form action="{{route('users.update',$user)}}" method="post" enctype="multipart/form-data">
-                @csrf
-                @method("PUT")
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input id="name" type="text" class="form-control" value="{{$user->name}}" name="name" placeholder="Enter name">
-                </div>
-                <div class="form-group">
-                    <label for="username">Username</label>
-                    <input id="username" type="text" value="{{$user->username}}" class="form-control" name="username" placeholder="Enter username">
-                </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input id="email" type="email" value="{{$user->email}}" class="form-control" name="email" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                    <label for="role">Role</label>
-                    <select name="role" id="role" class="form-control">
-                        @foreach ($roles as $role)
-                        <option {{$user->hasRole($role->name) ? 'selected' : ''}} >{{$role->name}}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input id="password" type="password" class="form-control" name="password" placeholder="Enter password">
-                </div>
-                <div class="form-group">
-                    <label for="confirm_password">Confirm Password</label>
-                    <input id="confirm_password" type="password" class="form-control" name="password_confirmation" placeholder="Repeat password">
-                </div>
-                <div class="form-group">
-                    <label for="avatar">Avatar</label>
-                    <input type="file" name="avatar" class="form-control" id="avatar">
-                </div>
-                <div class="form-group">
-                    <button class="btn btn-primary btn-block" type="submit">Submit</button>
-                </div>
-            </form>
+            <h4 class="card-title">Edit User</h4>
+            {!! form($form) !!}
         </div>
     </div>
 </div>

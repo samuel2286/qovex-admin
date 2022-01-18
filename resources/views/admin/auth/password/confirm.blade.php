e8c5dbe67f48da8a90a638a121fff76b48104d86
Musheabdulhakim <musheabdulhakim99@gmail.com>
2022-01-18 23:43:26 +0000

    confirm password

---
+++ resources/views/admin/auth/password/confirm.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.auth')
+
+@section('form')
+<form class="form-horizontal" method="post" action="{{route('password.reset')}}">
+    @csrf
+    <div class="alert alert-success text-center mb-4" role="alert">
+        Enter your Email and instructions will be sent to you!
+    </div>
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" id="email" value="{{old('email')}}" placeholder="johndoe@gmail.com" name="email">
+    </div>
+
+    <div class="mt-3">
+        <button class="btn btn-primary btn-block waves-effect waves-light"
+            type="submit">Reset</button>
+    </div>
+
+</form>
+@endsection
+
+@push('form-footer')
+<p>Remember It ? <a href="{{route('login')}}" class="font-weight-medium text-primary"> Sign In here</a> </p>
+@endpush

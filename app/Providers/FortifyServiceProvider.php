f3545332eef87a2921f1f6ee6ee3e23b31c313fe
Musheabdulhakim <musheabdulhakim99@gmail.com>
2022-01-18 23:35:53 +0000

    fortify service provider

---
+++ app/Providers/FortifyServiceProvider.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Http\Request;
+use Laravel\Fortify\Fortify;
+use App\Actions\Fortify\CreateNewUser;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use App\Actions\Fortify\ResetUserPassword;
+use App\Actions\Fortify\UpdateUserPassword;
+use Illuminate\Support\Facades\RateLimiter;
+use Laravel\Fortify\Contracts\LoginResponse;
+use App\Actions\Fortify\UpdateUserProfileInformation;
+
+class FortifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->instance(LoginResponse::class,new class implements LoginResponse {
+            public function toResponse($request){
+                return redirect()->route('dashboard');
+            }
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Fortify::loginView(function(){
+            $title =  'login';
+            return view('admin.auth.login',compact('title'));
+        });
+        Fortify::registerView(function(){
+            $title = 'register';
+            return view('admin.auth.register',compact('title'));
+        });
+        Fortify::requestPasswordResetLinkView(function(){
+            $title = 'forgot password';
+            return view('admin.auth.password.request',compact('title'));
+        });
+        Fortify::resetPasswordView(function($request){
+            $title = 'reset password';
+            return view('admin.auth.password.reset',compact('title'));
+        });
+        Fortify::confirmPasswordView(function () {
+            return view('admin.auth.password.confirm');
+        });
+        Fortify::createUsersUsing(CreateNewUser::class);
+        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
+        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+
+        RateLimiter::for('login', function (Request $request) {
+            $email = (string) $request->email;
+
+            return Limit::perMinute(5)->by($email.$request->ip());
+        });
+
+        RateLimiter::for('two-factor', function (Request $request) {
+            return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+    }
+}

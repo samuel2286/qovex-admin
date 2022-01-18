1dc36b3423d665dbff7ff2839eacb810cc432bc9
Musheabdulhakim <musheabdulhakim99@gmail.com>
2022-01-18 23:50:10 +0000

    logout route removed

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,7 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\BackupController;
 use App\Http\Controllers\Admin\SettingController;
 use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\Auth\LoginController;
 use App\Http\Controllers\Admin\PermissionController;
-use App\Http\Controllers\Admin\FilemanagerController;
-use App\Http\Controllers\Admin\Auth\RegisterController;
-use App\Http\Controllers\Admin\Auth\ForgotPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,38 +22,32 @@ use App\Http\Controllers\Admin\Auth\ForgotPasswordController;
 
 
 Route::prefix('admin')->group(function () {
-
     Route::middleware(['auth'])->group(function () {
-        Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
-        Route::get('',[DashboardController::class,'index']);
-        Route::post('logout',[LoginController::class,'logout'])->name('logout');
-        Route::get('login/locked', [LoginController::class,'locked'])->name('lockscreen');
-        Route::post('login/locked', [LoginController::class,'unlock'])->name('login.unlock');
+        Route::get('dashboard', [DashboardController::class,'index'])->name('dashboard');
+        Route::get('', [DashboardController::class,'index']);
+
     
-        Route::get('permissions',[PermissionController::class,'index'])->name('permissions');
-        Route::post('permissions',[PermissionController::class,'store']);
-        Route::put('permissions',[PermissionController::class,'update']);
-        Route::delete('permission',[PermissionController::class,'destroy'])->name('permission.destroy');
+        Route::get('permissions', [PermissionController::class,'index'])->name('permissions');
+        Route::post('permissions', [PermissionController::class,'store']);
+        Route::put('permissions', [PermissionController::class,'update']);
+        Route::delete('permission', [PermissionController::class,'destroy'])->name('permission.destroy');
     
-        Route::get('profile',[UserController::class,'profile'])->name('profile');
-        Route::post('profile/{user}/update-profile',[UserController::class,'updateProfile'])->name('profile.update');
-        Route::post('profile/{user}/change-password',[UserController::class,'updatePassword'])->name('profile.updatePassword');
-        Route::get('settings',[SettingController::class,'index'])->name('settings');
-        Route::post('settings',[SettingController::class,'store']);
+        Route::get('profile', [UserController::class,'profile'])->name('profile');
+        Route::post('profile/{user}/update-profile', [UserController::class,'updateProfile'])->name('profile.update');
+        Route::post('profile/{user}/change-password', [UserController::class,'updatePassword'])->name('profile.updatePassword');
+        Route::get('settings', [SettingController::class,'index'])->name('settings');
+        Route::post('settings', [SettingController::class,'store']);
     
         Route::resource('roles', RoleController::class);
-        Route::resource('users',UserController::class);
+        Route::resource('users', UserController::class);
     
         Route::get('backup', [BackupController::class,'index'])->name('backup.index');
         Route::put('backup/create', [BackupController::class,'create'])->name('backup.store');
         Route::get('backup/download/{file_name?}', [BackupController::class,'download'])->name('backup.download');
         Route::delete('backup/delete/{file_name?}', [BackupController::class,'destroy'])->where('file_name', '(.*)')->name('backup.destroy');
     });
-
-    
-
 });
 
-Route::get('/',function(){
+Route::get('/', function () {
     return view('welcome');
 });

9c2b98502edc43e6fd412921e3fe68d47c5fcb6a
Musheabdulhakim <musheabdulhakim99@gmail.com>
2022-01-18 23:41:23 +0000

    forgot password controller

--- app/Http/Controllers/Admin/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Admin/Auth/ForgotPasswordController.php
@@ -4,13 +4,35 @@ namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
     public function index(){
         $title = 'forgot password';
-        return view('admin.auth.password.forgot',compact(
+        return view('admin.auth.password.email',compact(
             'title'
         ));
     }
+
+    public function requestResetLink(Request $request){
+        $this->validate($request,['email' => 'required|email']);
+        $status = Password::sendResetLink($request->only('email'));
+        return $status === Password::RESET_LINK_SENT
+            ? back()->with(['status' => __($status)])
+            : back()->withErrors(['email' => __($status)]);
+    }
+
+    public function resetPasswordIndex(Request $request, $token){
+        return view('admin.auth.password.reset',compact($token));
+    }
+
+    public function resetPassword(Request $request){
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:6|confirmed',
+        ]);
+    }
 }
+ 

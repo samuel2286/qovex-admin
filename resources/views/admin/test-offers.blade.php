f7a7f9bbf921974b51553294ad74ece1660ddb6e
musheabdulhakim <musheabdulhakim99@gmail.com>
2023-08-12 00:00:02 +0000

    initial commit

--- resources/views/admin/test-offers.blade.php
+++ resources/views/admin/test-offers.blade.php
@@ -13,7 +13,7 @@
         <div class="card">
             <div class="card-body">
 
-                <h4 class="card-title">User Permission List</h4>
+                <h4 class="card-title">Test Offer List</h4>
 
                 <table id="datatable" class="table table-bordered dt-responsive nowrap"
                     style="border-collapse: collapse; border-spacing: 0; width: 100%;">
